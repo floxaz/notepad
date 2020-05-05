268d1a4ae536fb367b28880685f303dfb5dde2d0
Rostyslav Yatsura <floxaz1@gmail.com>
2020-05-05 19:43:06 +0200

    implemented handling of unhandled routes

--- backend/app.js
+++ backend/app.js
@@ -25,4 +25,13 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
+app.all('*', (req, res, next) => {
+  res.status(404);
+  res.json({
+    status: 'failire',
+    message: `Can't find ${req.originalUrl} on this server!`
+  });
+  next();
+})
+
 module.exports = app;
